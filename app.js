73bc3fd2bd04d6fab67b4d2529ecb0e8bf1f4d38
Nitrofski <Nitrofski@gmail.com>
2016-08-10 20:58:05 -0400

    This should work a little better.

--- app.js
+++ app.js
@@ -17,29 +17,28 @@ if (!accessToken) {
     + "&redirect_uri=" + redirectUri
     + "&response_type=" + responseType
     + "&scope=" + scope);
-}
-
-alert(accessToken);
-// We got an access token. We can now request the user queue.
-$.ajax({
-  url: "https://api.nightbot.tv/1/me",
-  type: "GET",
-  headers: { Authorization: "Bearer " + accessToken },
-  success: function (data) {
-    var channelName = data.user.displayName;
-    $('#req_url').text("beta.nightbot.tv/t/" + channelName + "/song_requests");
-  }
-});
-
-var $songRequest = $('#song_request');
-function update() {
+} else {
+  // We got an access token. We can now request the user queue.
   $.ajax({
-    url: "https://api.nightbot.tv/1/song_requests/queue",
+    url: "https://api.nightbot.tv/1/me",
     type: "GET",
     headers: { Authorization: "Bearer " + accessToken },
-    success: function(data) { alert(data); },
-    complete: function() { setTimeout(update, 3000); } // Schedule next call
+    success: function (data) {
+      var channelName = data.user.displayName;
+      $('#req_url').text("beta.nightbot.tv/t/" + channelName + "/song_requests");
+    }
   });
-}
 
-update();
+  var $songRequest = $('#song_request');
+  function update() {
+    $.ajax({
+      url: "https://api.nightbot.tv/1/song_requests/queue",
+      type: "GET",
+      headers: { Authorization: "Bearer " + accessToken },
+      success: function(data) { alert(data); },
+      complete: function() { setTimeout(update, 3000); } // Schedule next call
+    });
+  }
+
+  update();
+}
